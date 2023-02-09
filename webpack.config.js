739598aa83d22f619848f23d54491c5ee7be0172
Aliaksandr Bychyk <aliaksandr_bychyk@epam.com>
2023-02-09 18:26:10 +0100

    feat: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".ts", ".tsx", ".js", ".jxs"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    port: 3000
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
